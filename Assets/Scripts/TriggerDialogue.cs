1cf47232b3d111a4fc0134f089001c8f527ea53b
mintleaf <mintchipleaf@gmail.com>
2014-06-24 23:28:05 -0400

    Add dialogue text triggering

---
+++ Assets/Scripts/TriggerDialogue.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDialogue : MonoBehaviour {
+
+	private bool display;
+	private GameObject speaker;
+	private Transform speakerTransform;
+	private Vector3 position;
+	private Camera cam;
+
+	void Start() {
+		speaker = gameObject;
+		speakerTransform = speaker.transform;
+		print (speakerTransform.ToString());
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		display = true;
+		cam = Camera.current;
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		position = cam.WorldToScreenPoint(speakerTransform.position);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		display = false;
+	}
+
+	/*IEnumerator ShowMessage (string message, float delay) {
+		guiText.text = message;
+		guiText.enabled = true;
+		yield return new WaitForSeconds(delay);
+		guiText.enabled = false;
+	}*/
+	
+	void OnGUI() {
+		if(display == true) {
+			print (position.y.ToString());
+			GUI.Label(new Rect (position.x, position.y, 100, 20), "Hello World");
+
+		}
+	}
+
+}
